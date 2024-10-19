c1c74feeb3a84f0210f953ffbfe719220f6052a0
Lilian Bischung <bischung@unistra.fr>
2024-10-19 19:02:54 +0200

    another fix for vercel

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!n*c@(8gc*6(f%7f67-*p6!(qogwt8ql$3x#%865g*l92#*itb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '*.vercel.app']
 
 
 # Application definition
