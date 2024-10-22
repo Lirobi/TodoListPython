b4bdacae8f067cccb1ed67b5e204b101c563b837
Lilian Bischung <bischung@unistra.fr>
2024-10-22 11:07:29 +0200

    fixed

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
