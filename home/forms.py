17a41e18b368c47b0ba752be433c76562b18ec98
Lilian Bischung <bischung@unistra.fr>
2024-10-22 17:17:44 +0200

    Système de compte terminé

--- home/forms.py
+++ home/forms.py
@@ -1,16 +1,18 @@
 from django import forms
+from django.contrib.auth import models
 from .models import TodoItem
 
 class TodoItemForm(forms.ModelForm):
     id = forms.IntegerField(label="")
     id.widget = id.hidden_widget()
     name = forms.CharField(max_length=150, label="Name", label_suffix="")
-    name.widget.attrs = {"size": "50"}
+    name.widget.attrs = {"size": "50", "class": "scale-animation scale-animation-1-02"}
     completed = forms.BooleanField(required=False, label="Completed", label_suffix="")
-    completed.widget.attrs = {"onChange": "save.click()"}
+    completed.widget.attrs = {"onChange": "save.click()", "class": "scale-animation scale-animation-1-1"}
     
     
     class Meta:
         model = TodoItem
         fields = ["id", "name", "completed"]
+
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, redirect
 from .models import TodoItem
 from .forms import TodoItemForm
 from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -9,55 +13,105 @@ def home(request):
     return render(request, "home.html")
 
 def todos(request):
-    return render(request, "todos.html", {"todos": getTodos()})
+    if not request.user.is_authenticated:
+        return redirect("login")
+    return render(request, "todos.html", {"todos": getTodos(request.user)})
 
 def update(request):
-    if request.method == "POST":
+    if not request.user.is_authenticated:
+        return redirect("login")
+    if request.method == "POST": #check if the request type is good
         if "id" in request.POST and "name" in request.POST:
             id = request.POST["id"]
             name = request.POST["name"]
             completed = False
 
             if len(name) >= 150:
-                return HttpResponseRedirect("../todos")
+                return redirect("todos")
 
             if "completed" in request.POST and request.POST["completed"] == "on":
                 completed = True
             
 
             if "save" in request.POST:
-                todoItem = TodoItem(id=id, name=name, completed=completed)
+                todoItem = TodoItem(id=id, name=name, completed=completed, user=request.user.username)
                 todoItem.save()
 
             elif "delete" in request.POST:
                 todoItem = TodoItem.objects.get(id=id)
                 todoItem.delete()
+        else:
+            return render(request, "todos.html", {"error": "Unexpected error..."})
+    else: 
+        return render(request, "todos.html", {"error": "Invalid request"})
+    
+    return redirect("todos")
 
 
-    return HttpResponseRedirect("../todos")
 
-def account(request):
+
+
+def login(request):# if logged in : go to account management
+    if request.user.is_authenticated:
+        return redirect("../todos")
     if request.method == "POST":
-        if "login" in request.POST and "password" in request.POST:
-            login = request.POST["login"]
+        if "login" in request.POST and "password" in request.POST: #if request not empty
+            login_ = request.POST["login"]
             password = request.POST["password"]
 
-            user = authenticate(username=login, password=password)
-            if user is not None:
-                pass
+            user = authenticate(username=login_, password=password)
+            if user is not None and user.is_active:
+                auth.login(request, user)
+                return redirect("../todos")#go to todos
+                
             else:
-                return render(request, "account.html", {"error" : "Error while authenticating, please retry later."})
+                return render(request, "account.html", {"error" : "Error while authenticating, please retry later."}) # if unknow error
+        
+        
         else:
-            return render(request, "account.html", {"error" : "Invalid credentials."})
+            missing = ""
+            if "password" in request.POST:
+                missing = "Please enter username."
+            else:
+                missing = "Please enter password."
+            return render(request, "login.html", {"message": missing}) #if request is not good
     else:
-        return render(request, "account.html", {"error": "Invalid request."})
-    return HttpResponseRedirect("../todos")
+      return render(request, "login.html")  
 
-#TODO : faire la connection user, associer un userid à un todo. ne charger que les todos qui ont l'user id de l'utilisateur connecté
+def register(request):
+    if request.method == "POST":
+        if "email" in request.POST and "username" in request.POST and "password" in request.POST:
+            email = request.POST["email"]
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = User.objects.create_user(email=email, username=username, password=password)
+            try:
+                user.save()
+            except Exception:
+                return render(request, "register.html", {"error": str(Exception)})
+            user = authenticate(username=username, password=password)
+            auth.login(request, user)
+
+            return redirect("todos")
+        else:
+            missing = "Missing values"
+            return render(request, "register.html", {"message": missing}) #if request is not good
+    
+    return render(request, "register.html")
 
 
 
 
+def account(request):
+    if request.user is not None:
+        return render(request, "account.html")
+    return redirect("login")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+    
 
 
 
@@ -67,13 +121,11 @@ def account(request):
 #UTILS
 
 
-def getTodos(): #TODO Mettre user en paramètre et retourner ceux de cet utilisateur
+def getTodos(user): #TODO Mettre user en paramètre et retourner ceux de cet utilisateur
     todos = []
-    for item in TodoItem.objects.all():
+    for item in TodoItem.objects.all().filter(user = user.username):
         form = TodoItemForm(instance=item)
         todos.append(form)
-
-    
 
     idList = TodoItem.objects.values_list('id', flat=True)
     newId = -1
@@ -83,7 +135,6 @@ def getTodos(): #TODO Mettre user en paramètre et retourner ceux de cet utilisa
             newId = i
         i+=1
         
-
     newItem = TodoItem(id=newId)
 
     todos.append(TodoItemForm(instance=newItem))
